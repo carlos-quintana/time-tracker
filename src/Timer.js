dbda78eb47aaa0e3c36b88c619b2c0085ec5edef
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2022-04-27 23:00:04 -0500

    Removed unnecesary logging to reduce complexity

--- src/Timer.js
+++ src/Timer.js
@@ -5,13 +5,9 @@ export default function Timer() {
     const [currentCount, setCurrentCount] = useState(0);
 
     useEffect(() => {
-        console.log("Inside use effect")
-        let intervalTimer = setInterval(() => {
-            setCurrentCount(prevTime => {
-                              console.log(`Inside the interval with a count of ${prevTime}`);
-                              return prevTime + 1;
-                           })
-        }, 1000)
+        let intervalTimer = setInterval(() => 
+            setCurrentCount(prevTime => prevTime + 1)
+        , 1000)
         return () => clearInterval(intervalTimer);
     }, [])
 
