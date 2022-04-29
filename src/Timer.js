bc1a86e3ae0371a8ded0576d2d80d62e01a3dd4d
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2022-04-28 23:29:16 -0500

    Implemented timer controls

--- src/Timer.js
+++ src/Timer.js
@@ -3,13 +3,30 @@ import { useEffect, useState } from 'react'
 export default function Timer() {
 
     const [currentCount, setCurrentCount] = useState(0);
+    const [isTimerActive, setIsTimerActive] = useState(false);
 
     useEffect(() => {
-        let intervalTimer = setInterval(() => 
-            setCurrentCount(prevTime => prevTime + 1)
-        , 1000)
+        let intervalTimer = null;
+        if (isTimerActive)
+            intervalTimer = setInterval(() => {
+                setCurrentCount(prevTime => prevTime+10)
+            }, 10)
+        else clearInterval(intervalTimer)
         return () => clearInterval(intervalTimer);
-    }, [])
+    }, [isTimerActive])
 
-    return (<p>{currentCount}</p>);
+    return (
+        <>
+            <p>{currentCount}</p>
+            <button onClick={() => setIsTimerActive(true)}>
+                Start
+            </button>
+            <button onClick={() => setIsTimerActive(false)}>
+                Pause
+            </button>
+            <button onClick={() => { setCurrentCount(0); setIsTimerActive(false) }}>
+                Reset
+            </button>
+        </>
+    );
 }
