b21a1cc4d0eb971613f239b4687d165af6551153
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2022-04-30 18:54:42 -0500

    Gave the current time the correct hh:mm:ss format

--- src/Timer.js
+++ src/Timer.js
@@ -5,19 +5,29 @@ export default function Timer() {
     const [currentCount, setCurrentCount] = useState(0);
     const [isTimerActive, setIsTimerActive] = useState(false);
 
+    function formatTime(secondsCount) {
+        let hours = Math.floor(secondsCount / 3600);
+        let minutes = Math.floor(secondsCount / 60) % 60;
+        let seconds = secondsCount % 60;
+        hours = hours < 10 ? `0${hours}` : hours;
+        minutes = minutes < 10 ? `0${minutes}` : minutes;
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
+        return `${hours}:${minutes}:${seconds}`
+    }
+
     useEffect(() => {
         let intervalTimer = null;
         if (isTimerActive)
             intervalTimer = setInterval(() => {
-                setCurrentCount(prevTime => prevTime+10)
-            }, 10)
+                setCurrentCount(prevTime => prevTime + 1)
+            }, 1000)
         else clearInterval(intervalTimer)
         return () => clearInterval(intervalTimer);
     }, [isTimerActive])
 
     return (
         <>
-            <p>{currentCount}</p>
+            <p>{formatTime(currentCount)}</p>
             <button onClick={() => setIsTimerActive(true)}>
                 Start
             </button>
