ae54072b907ffe15d7322bcc2bf9315d524a2cb8
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2022-05-11 17:42:35 -0500

    Defined initial application structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import InputPanel from "./InputPanel"
+import ListPanel from "./ListPanel"
+
+export default function App() {
+  return (
+    <>
+      <h1>Time Tracker Application</h1>
+      < InputPanel />
+      < ListPanel />
+    </>
+  );
+}
