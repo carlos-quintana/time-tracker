8d351e93d146451a4bc5a68a971e463301b0ddb2
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2022-05-27 14:45:06 -0500

    Defined the  list of entries as state of the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
+import { useState, useEffect } from "react";
 import InputPanel from "./InputPanel"
 import ListPanel from "./ListPanel"
 
 export default function App() {
+
+  /*  Entries in the list have the form:
+      { id: number, description: string, secondsCount: number }  */
+  const [entriesList, setEntriesList] = useState([])
+
   return (
     <>
       <h1>Time Tracker Application</h1>
