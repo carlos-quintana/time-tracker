dc2b6eea26045d325997c13ec8052bbc87675056
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-01-23 22:22:29 -0500

    Functional timer on screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 export default function App() {
+  // Holds the current amount of seconds the timer has counted
+  const [seconds, setSeconds] = useState(0)
+  // Create an interval when the component is rendered and clear it whenever it's deleted
+  useEffect(() => {
+    let timerSetInterval = setInterval(() => {
+      setSeconds(prevTime => prevTime + 1)
+    }, 1000);
+    return () => clearInterval(timerSetInterval);
+  }, [])
+
   return (
-    <h2>Time Tracker application</h2>
+    <div>
+      <h2>Time Tracker application</h2>
+
+      <p>Seconds elapsed: {seconds}</p>
+    </div>
   )
 }
