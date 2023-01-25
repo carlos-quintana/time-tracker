438e12a0d8cac087440f384e823972fd86762525
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-01-24 19:02:29 -0500

    Debbuging constants and documentation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react"
 
+
+// This next variables are used for debugging the timer
+const TIMER_INCREMENT = 1
+const TIMER_INTERVAL_MS = 25 // TODO: Reset to 1000 ms
+
+// TODO: Break down the App component into the major subcomponents: Input and Display
 export default function App() {
   // Holds the current amount of seconds the timer has counted
   const [seconds, setSeconds] = useState(0)
@@ -12,9 +18,9 @@ export default function App() {
     let timerSetInterval = null;
     if (timerStatus === "running") {
       timerSetInterval = setInterval(() => {
-        setSeconds(prevTime => prevTime + 1)
-      }, 1000);
-    } else 
+        setSeconds(prevTime => prevTime + TIMER_INCREMENT)
+      }, TIMER_INTERVAL_MS);
+    } else
       clearInterval(timerSetInterval)
     return () => clearInterval(timerSetInterval);
   }, [timerStatus])
@@ -40,12 +46,13 @@ export default function App() {
   return (
     <div>
       <h2>Time Tracker application</h2>
-
+      {/* Timer Display */}
       <p>Seconds elapsed: {seconds}</p>
-
+      {/* Timer Controls */}
       <button onClick={startTimer}>Start</button>
       <button onClick={stopTimer}>Stop</button>
       <button onClick={resetTimer}>Reset</button>
+      {/* New Task Input */}
       <form onSubmit={handleSubmitNewTask}>
         <input id="newTaskInput" name="newTaskInput" type="text" value={newTask} onChange={handleChangeNewTask} placeholder="Input your task"/>
         <input id="submitNewTask" name="submitNewTask" type="submit" value="Submit" />
