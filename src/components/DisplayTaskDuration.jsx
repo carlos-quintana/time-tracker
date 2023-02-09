b96cbc773876d87705005864b34506bcf2c604b4
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-02-09 17:58:20 -0500

    Substitute the input tag for a component
    Replaced the HTML input tag for a component made to represent a controlled text input, where the user is constrained to a 'HH:MM:SS' format which represents an arbitrary amount of time, up to ~100 hours.

--- src/components/DisplayTaskDuration.jsx
+++ src/components/DisplayTaskDuration.jsx
@@ -1,64 +1,63 @@
-import React, { useState } from "react"
-import { secondsToFormattedHMS } from "../helpers/timeFormatting"
+import React, { useEffect, useState } from "react"
+import { secondsToFormattedHMS, formattedHMSToSeconds } from "../helpers/timeFormatting"
+import InputTimeCustom from "./InputTimeCustom"
+
+const calculateSeconds = (start, end) => Math.floor((end - start) / 1000);
 
 const DisplayTaskDuration = ({ id, interval: { start, end }, handleIntervalUpdate }) => {
 
-    const duration = Math.floor((end - start) / 1000)
-
-    const [tempDuration, setTempDuration] = useState(duration)
+    // This will be a Number to keep track of the seconds corresponding to the duration of the interval, the number will be used to calculate the HMS string and this in turn will be provided to the custom input component.
+    const [durationSeconds, setDurationSeconds] = useState(calculateSeconds(start, end))
     const [isEditingDuration, setIsEditingDuration] = useState(false)
 
+    // This updates the HMS to display whenever the interval is updated
+    useEffect(() => setDurationSeconds(calculateSeconds(start, end)), [start, end])
+
     const handleSubmit = event => {
+        // console.log("> Inside of handle Submit")
         event.preventDefault()
-        // console.log(`An edit for the duration has been submitted for the task ${id}`)
-        // Form validations
-        if (tempDuration === duration) {
+        console.log(`An edit for the duration has been submitted for the task ${id}`)
+        console.log(event)
+        console.log(event.target)
+        console.log(event.target.value)
+        let inputDurationString = event.target.value;
+        // Validate the string is of the form H+:MM:SS
+        if (!((/^\d+:[012345]\d:[012345]\d$/).test(inputDurationString))) {
+            alert("An error has ocurred, the inputted duration is not valid")
             setIsEditingDuration(false)
             return
         }
-        if (tempDuration === "") {
-            alert("The duration cannot be empty") // #TODO Implement a better notification
+        if (formattedHMSToSeconds(inputDurationString)===0) {
+            alert("The duration cannot be zero")
+            setIsEditingDuration(false)
             return
+
         }
         // Edit submission
-        // console.log("About to fire the handleIntervalUpdate")
-        handleIntervalUpdate({ start, end: (start + tempDuration * 1000) })
+        handleIntervalUpdate({ start, end: (start + formattedHMSToSeconds(inputDurationString) * 1000) })
         // Cleanup
         setIsEditingDuration(false)
+        // console.log("< Outside of handle Submit")
     }
 
     return (
-        <>
-            {
-                isEditingDuration ?
-                    <form onSubmit={event => handleSubmit(event)}>
-                        <input
-                            id={`${id}-editTaskDuration`}
-                            name="editTaskDuration"
-                            type="text"
-                            value={tempDuration}
-                            onChange={event => setTempDuration(event.target.value)}
-                            autoFocus
-                            onBlur={event => handleSubmit(event)}
-                        />
-                    </form>
-                    :
-
-                    <button
-                        onClick={() => setIsEditingDuration(true)}
-                    >
-                        <span>
-                            {secondsToFormattedHMS(duration)}
-                        </span>
-                    </button>
-            }
-            {/* <button
+        <>{isEditingDuration ?
+            <form onSubmit={event => handleSubmit(event)}>
+                <InputTimeCustom
+                    id={id}
+                    durationSeconds={durationSeconds}
+                    handleSubmit={handleSubmit}
+                />
+            </form>
+            :
+            <button
                 onClick={() => setIsEditingDuration(true)}
-                disabled={isEditingDuration}
             >
-                Editar duración
-            </button> */}
-        </>
+                <span>
+                    {secondsToFormattedHMS(durationSeconds)}s
+                </span>
+            </button>
+        }</>
     )
 }
 
