17353d1108ea59f02ebc55202433621d7c760167
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-04-15 14:08:03 -0500

    Implemented the first warning modal for reset all data

--- src/components/ControlsPanel/ControlsPanel.jsx
+++ src/components/ControlsPanel/ControlsPanel.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import { useModal } from "../../hooks/useModal";
+import Modal from "../Shared Components/Modal";
 
 const ControlsPanel = () => {
 
-    // Modal
+    let [isOpenModal, openModal, closeModal] = useModal(false);
 
-    /**
-     * This function reloads the application and restores all of the dummy data in the example files. We do this by clearing the Local Storage, which the application will interpret as a new user entering for the first time.
-     */
+    /** This function reloads the application and restores all of the dummy data in the example files. We do this by clearing the Local Storage, which the application will interpret as a new user entering for the first time. */
     const resetData = () => {
         localStorage.clear()
         window.location.reload()
@@ -14,10 +14,21 @@ const ControlsPanel = () => {
 
     return (
         <>
-            <button className="button button-warning" onClick={resetData} >
+            <button className="button button-warning" onClick={openModal} >
                 Reset data
             </button>
-            <span> This will reset all Tasks and Projects to the example data.</span>
+
+            <Modal
+                isOpen={isOpenModal}
+                closeModal={closeModal}
+                modalTitle="Warning"
+                hasConfirmation={true}
+                onConfirmationCallback={resetData}
+            >
+                <p>This will reset all Tasks and Projects to the example data.</p>
+            </Modal>
+
+
         </>
     );
 }
