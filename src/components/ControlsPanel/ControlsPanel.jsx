0c7269532437ae539de706582f3da3ea6bfeae4d
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-04-14 23:56:58 -0500

    Uninstalled Chakra UI
    While the pre-built components were looking promising, the library ended up being too restrictive and the amount of work that it would take to slowly refactor the whole component and styles codebase to fit in nicely with Chakra is definitely not worth it for the time being. Will move on to other solutions like Headless UI or going to to using Popper manually for the popovers and tooltipos

--- src/components/ControlsPanel/ControlsPanel.jsx
+++ src/components/ControlsPanel/ControlsPanel.jsx
@@ -1,21 +1,8 @@
 import React from "react";
 
-import {
-    useDisclosure,
-    Button,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-} from '@chakra-ui/react'
-
 const ControlsPanel = () => {
 
     // Modal
-    const { isOpen, onOpen, onClose } = useDisclosure()
 
     /**
      * This function reloads the application and restores all of the dummy data in the example files. We do this by clearing the Local Storage, which the application will interpret as a new user entering for the first time.
@@ -27,27 +14,10 @@ const ControlsPanel = () => {
 
     return (
         <>
-            <button className="button button-warning" onClick={onOpen} >
+            <button className="button button-warning" onClick={resetData} >
                 Reset data
             </button>
             <span> This will reset all Tasks and Projects to the example data.</span>
-
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Warning</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        This action will reset the application and replace all the current tasks, projects and other data with the initial example. Do you want to continue?
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme='red' mr={3} onClick={resetData}>
-                            Reset data
-                        </Button>
-                        <Button variant='ghost' onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
         </>
     );
 }
