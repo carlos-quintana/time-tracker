9de014ed2481b17cee037bc709898980e0348f75
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-04-20 10:49:12 -0500

    Added optional flipping variable
    As there are edge cases where the popover is on the top of the page it will be necessary to leverage the Popper library and let the floating element flip it's direction. As this is a very rare case it's left as an optional paramenter.

--- src/hooks/usePopover.jsx
+++ src/hooks/usePopover.jsx
@@ -4,13 +4,13 @@ import { usePopper } from "react-popper";
 /**
  * This hook works alongside the Popover component, which represents as very small floating window that appears on top of a designated element and contains a small message. It's like a modal but less intrusive, and can be used to deliver messages, ask for confirmation for an action, etc. The parent component that will utilize this custom hook to initialize a series of state variables and DOM refs that the Popover component needs. This hook and said component, rely heavily on the use of React Popper. For more information visit the docs: https://popper.js.org/react-popper/v2/ 
  */
-export default function usePopover() {
+export default function usePopover(isFlippingEnabled=false) {
     /** This relates to the popover that will appear over the delete button when clicked */
     const [isOpenPopover, setIsOpenPopover] = useState(false);
     const [refFocusElement, setRefFocusElement] = useState(null);
     const [refPopperElement, setRefPopperElement] = useState(null);
     const { styles, attributes } = usePopper(refFocusElement, refPopperElement, {
-        placement: 'top', modifiers: [{ name: 'flip', enabled: false }]
+        placement: 'top', modifiers: [{ name: 'flip', enabled: isFlippingEnabled }]
     });
 
     /** With this ref we'll be able to add a Listener to the document to know when the user clicks outside of the popover and close it */
