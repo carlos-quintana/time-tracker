7592a3b5b7c6924dfd51ced0587831941c968cea
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-04-28 19:45:08 -0500

    Extracted milestones function into a separate file

--- src/components/TasksPanel/PanelTasks.tsx
+++ src/components/TasksPanel/PanelTasks.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import TaskRow from "./TaskRow";
 import { Task, Project, CurrentTask } from "../../types";
+import mapTasksToMilestone from "../../helpers/task-milestones/milestones";
 
 type Props = {
     tasksList: Task[],
@@ -24,12 +25,16 @@ type Props = {
  */
 const PanelTasks = ({ tasksList: givenTasks, editTask, deleteTask, currentTask, setCurrentTask, projectsList, createProject }: Props) => {
 
-    const [mappedTasks, setMappedTasks] = useState({});
+    const [mappedTasks, setMappedTasks] = useState<Object>({});
     const currentDate = useRef(new Date());
 
     useEffect(() => {
+        console.log(`Going to set Mapped Tasks`)
         setMappedTasks(
-            mapTasksToMilestone( // Set the organized tasks to display to an object that has them organized into milestones
+            mapTasksToMilestone( 
+                // Pass in the date which the milestones will be calculated around
+                currentDate.current,
+                // Set the organized tasks to display to an object that has them organized into milestones
                 givenTasks.sort((a, b) => { // But first order the list of tasks
                     // Compare the 2 dates
                     if (a.interval.start > b.interval.start) return -1;
@@ -39,73 +44,6 @@ const PanelTasks = ({ tasksList: givenTasks, editTask, deleteTask, currentTask, 
             )
         )
     }, [givenTasks])
-
-    /**
-     * TODO: Take this (and the few subsequent) function(s) out into another file and write tests.
-     * TODO: Look into changing the returned Object to an Array of Objects
-     * This function takes an ordered list of Tasks (by starting timestamp) and categorizes them into different established milestones like Today, Yesterday, This Week, etc.
-     * For this we go through each task and check if it's starting timestamp belongs to any of these milestones using different functions that compare two timestamps. Each milestone also contains a String thast will be displayed as a title of the section.
-     * @param {Array<Task>} tasksList - The list of tasks to be categorized.
-     * @returns {Object} An object containing the different milestones extracted from the tasks, each containing a title and the list of tasks.
-     */
-    const mapTasksToMilestone = (tasksList: Task[]) => {
-        let newMappedTasks = {};
-        tasksList.forEach(task => {
-            let taskStart = new Date(task.interval.start)
-            if (isSameDay(currentDate.current, taskStart)) {
-                // @ts-ignore
-                if (!newMappedTasks.today)
-                    // @ts-ignore
-                    newMappedTasks.today = { tasks: [], title: "Today" }
-                // @ts-ignore
-                newMappedTasks.today.tasks.push(task)
-                return
-            }
-            if (isYesterday(currentDate.current, taskStart)) {
-                // @ts-ignore
-                if (!newMappedTasks.yesterday)
-                    // @ts-ignore
-                    newMappedTasks.yesterday = { tasks: [], title: "Yesterday" }
-                // @ts-ignore
-                newMappedTasks.yesterday.tasks.push(task)
-                return
-            }
-            if (isSameWeek(taskStart, currentDate.current)) {
-                // @ts-ignore
-                if (!newMappedTasks.thisWeek)
-                    // @ts-ignore
-                    newMappedTasks.thisWeek = { tasks: [], title: "This Week" }
-                // @ts-ignore
-                newMappedTasks.thisWeek.tasks.push(task)
-                return
-            }
-            else {
-                // @ts-ignore
-                if (!newMappedTasks.older)
-                    // @ts-ignore
-                    newMappedTasks.older = { tasks: [], title: "Older" }
-                // @ts-ignore
-                newMappedTasks.older.tasks.push(task)
-                return
-            }
-        })
-        return newMappedTasks;
-    }
-
-    // @ts-ignore
-    const isSameDay = (dateA, dateB) => {
-        return dateA.getDate() === dateB.getDate() && dateA.getMonth() === dateB.getMonth() && dateA.getFullYear() === dateB.getFullYear()
-    }
-
-    // @ts-ignore
-    const isYesterday = (dateA, dateB) => {
-        return dateA.getDate() - 1 === dateB.getDate() && dateA.getMonth() === dateB.getMonth() && dateA.getFullYear() === dateB.getFullYear()
-    }
-
-    // @ts-ignore
-    const isSameWeek = (dateA, dateB) => {
-        return dateA.valueOf() >= dateB.valueOf() - 1000 * 60 * 60 * 24 * 7
-    }
 
     return (
         <section>
