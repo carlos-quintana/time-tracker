2b315b66c99f4c4e11434fd96adadaa547870e53
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-05-07 01:09:30 -0500

    Redid the footer component styles

--- src/components/Pages/Layout/Footer.tsx
+++ src/components/Pages/Layout/Footer.tsx
@@ -1,21 +1,26 @@
 // Material Icons
 import GitHubIcon from '@mui/icons-material/GitHub';
-import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import WorkIcon from '@mui/icons-material/Work';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer = () => {
     return (
         <footer className="footer">
             <h4>This demo was created by Carlos Quintana</h4>
-            <a href="https://carlos-quintana.github.io/" target="_blank" rel="noreferrer">
-                <WorkOutlineIcon /> See my Web Portfolio and other Projects
-            </a>
-            <a href="https://github.com/carlos-quintana/" target="_blank" rel="noreferrer">
-                <GitHubIcon /> See my GitHub Page
-            </a>
-            <a href="https://www.linkedin.com/in/carlos-quintana-dev/" target="_blank" rel="noreferrer">
-                <LinkedInIcon /> Let's get in touch through LinkedIn
-            </a>
+            <div className="footer-links">
+                <a href="https://carlos-quintana.github.io/" target="_blank" rel="noreferrer">
+                    <WorkIcon /> Web Portfolio
+                </a>
+                <a href="https://github.com/carlos-quintana/time-tracker" target="_blank" rel="noreferrer">
+                    <GitHubIcon /> This repository
+                </a>
+                <a href="https://github.com/carlos-quintana/" target="_blank" rel="noreferrer">
+                    <GitHubIcon /> My GitHub
+                </a>
+                <a href="https://www.linkedin.com/in/carlos-quintana-dev/" target="_blank" rel="noreferrer">
+                    <LinkedInIcon /> LinkedIn
+                </a>
+            </div>
         </footer>
     )
 };
