360002ceb3aeef6ac3416da524f276e4cb6d7d4b
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-05-06 23:37:20 -0500

    Overhauled the header and nav bar

--- src/components/Pages/Layout/Layout.tsx
+++ src/components/Pages/Layout/Layout.tsx
@@ -1,14 +1,14 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
-import Nav from "./Nav";
 
 const Layout = () => {
     return (
         <>
             <Header />
-            <Nav />
-            <Outlet />
+            <div className="content">
+                <Outlet />
+            </div>
             <Footer />
         </>
     );
