08a18307e35826e80de4d7be19dc16efd4cd7362
Carlos Quintana <carlosd.quintana.dev@gmail.com>
2023-05-06 22:14:12 -0500

    Implemented routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ import PanelProjects from "./Pages/Projects/PanelProjects"
 import ControlsPanel from "./Pages/Configuration/ControlsPanel"
 // Data Access Logic
 import useDataAccess from "../hooks/useDataAccess";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 /**
  * This is the main component for the application and the one that is at the top of the components hierarchy. This means that this will hold the state for the data of the application until a state manager is implemented, like Context or Redux.
@@ -36,6 +37,65 @@ export default function App() {
       />
       <ControlsPanel />
       <Footer />
+
+
+      {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={
+            <TasksPage
+              tasksList={tasksList} createNewTask={createNewTask} editTask={editTask} deleteTask={deleteTask}
+              currentTask={currentTask} setCurrentTask={setCurrentTask}
+              projectsList={projectsList} createProject={createProject} />
+          } />
+          <Route path="projects" element={
+            <PanelProjects
+              tasksList={tasksList}
+              projectsList={projectsList} createProject={createProject}
+              editProject={editProject} deleteProject={deleteProject}
+            />
+          } />
+          <Route path="options" element={<ControlsPanel />} />
+          <Route path="about" element={
+            <><p>Coming soon...</p></>
+          } />
+          <Route path="*" element={
+            <>
+              <p><strong>404</strong> There's nobody here</p>
+            </>
+          } />
+        </Route>
+      </Routes>
+
+
     </>
   )
+}
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/options">Options</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
